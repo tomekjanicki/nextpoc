88c7889e48dfdf8025be68aafa4912f86072322d
Tomasz Janicki <tomasz.janicki@ciber.com>
2017-06-08 07:44:53 +0200

    xxx

--- Projects/Next.WTR.Web.Client.TestApp/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-[assembly: AssemblyTitle("Next.WTR.Web.Client.TestApp")]
-[assembly: AssemblyProduct("Next.WTR.Web.Client.TestApp")]
--- Projects/Next.WTR.Web.Client/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-[assembly: AssemblyTitle("Next.WTR.Web.Client")]
-[assembly: AssemblyProduct("Next.WTR.Web.Client")]
