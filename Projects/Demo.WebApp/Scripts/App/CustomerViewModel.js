c889f86aaf4e5da043153b6f68e82da125545164
Tomasz Janicki <tomasz.janicki@ciber.com>
2017-06-08 15:43:07 +0200

    xxx

--- Projects/Demo.WebApp/Scripts/App/CustomerViewModel.js
+++ Projects/Demo.WebApp/Scripts/App/CustomerViewModel.js
@@ -8,6 +8,17 @@
     self.version = version;
 }
 
+function ObservableCustomer(id, name, surname, phoneNumber, address, version) {
+    var self = this;
+    self.id = window.ko.observable(id);
+    self.name = window.ko.observable(name);
+    self.surname = window.ko.observable(surname);
+    self.phoneNumber = window.ko.observable(phoneNumber);
+    self.address = window.ko.observable(address);
+    self.version = window.ko.observable(version);
+}
+
+
 function CustomerViewModel(baseUrl) {
     var self = this;
 
@@ -18,14 +29,14 @@ function CustomerViewModel(baseUrl) {
 
     self.customers = window.ko.observableArray([]);
 
-    self.testClick = function () {
-        alert("ala ma kota");
-        ajaxGet("customers/?top=100&skip=0",
+    self.customer = window.ko.observable(null);
+
+    self.initCustomers = function () {
+        ajaxGetOrDelete("GET", "customers/?top=1000&skip=0",
             function (data) {
-                alert("ok");
-                var x = data.items;
+                var itemList = data.items;
                 var items = [];
-                $.each(x, function (i, item) {
+                $.each(itemList, function (i, item) {
                     items.push(new Customer(item.id, item.name, item.surname, item.phoneNumber, item.address, item.version));
                 });
                 self.customers(items);
@@ -35,11 +46,53 @@ function CustomerViewModel(baseUrl) {
             }
     }
 
-    function ajaxGet(endpoint, doneFn, failFn) {
+    self.deleteCustomer = function(customer) {
+        ajaxGetOrDelete("DELETE", "customers/" + customer.id + "?version=" + customer.version,
+            function() {
+                self.customers.remove(customer);
+            },
+            function() {
+                alert("error");
+            });
+    }
+
+    self.backToList = function() {
+        self.listVisible(true);
+        self.insertOrUpdateVisible(false);        
+    }
+
+    self.editCustomer = function(customer) {
+        ajaxGetOrDelete("GET", "customers/" + customer.id,
+            function (data) {
+                var observableCustomer = new ObservableCustomer(data.id, data.name, data.surname, data.phoneNumber, data.address, data.version);
+                self.customer(observableCustomer);
+                self.listVisible(false);
+                self.insertOrUpdateVisible(true);
+            },
+            function () {
+                alert("error");
+            });
+    }
+
+    function ajaxGetOrDelete(method, endpoint, doneFn, failFn) {
         $.ajax({
-            method: "GET",
+            method: method,
             url: self.baseUrl + endpoint,
             cache: false,
+            contentType: "application/json; charset=UTF-8"
+        }).done(function (result) {
+            doneFn(result);
+        }).fail(function () {
+            failFn();
+        });
+    }
+
+    function ajaxPostOrPut(method, endpoint, data, doneFn, failFn) {
+        $.ajax({
+            method: method,
+            cache: false,
+            url: self.baseUrl + endpoint,
+            data: data,
             contentType: "application/json; charset=UTF-8"
         }).done(function (result) {
             doneFn(result);
@@ -51,5 +104,6 @@ function CustomerViewModel(baseUrl) {
 
 $(function () {
     var vm = new CustomerViewModel("http://localhost:2776/");
+    vm.initCustomers();
     window.ko.applyBindings(vm);
 })
