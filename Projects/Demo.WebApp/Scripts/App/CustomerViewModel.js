d4aa5dd2307ca6a3981677ea910b719787ef7bb9
Tomasz Janicki <tomasz.janicki@ciber.com>
2017-06-08 13:46:20 +0200

    xxx

---
+++ Projects/Demo.WebApp/Scripts/App/CustomerViewModel.js
@@ -0,0 +1,14 @@
+﻿function CustomerViewModel(baseUrl) {
+    var self = this;
+
+    self.baseUrl = baseUrl;
+
+    self.testClick = function() {
+        alert("ala ma kota");
+    }
+}
+
+$(function () {
+    var vm = new CustomerViewModel("http://localhost:2776/");
+    window.ko.applyBindings(vm);
+})
