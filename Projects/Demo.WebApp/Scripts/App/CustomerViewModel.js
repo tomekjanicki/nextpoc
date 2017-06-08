3a2917cf3a878ffca8074fb55b68512c8d4f4777
Tomasz Janicki <tomasz.janicki@ciber.com>
2017-06-08 14:34:10 +0200

    xxx

--- Projects/Demo.WebApp/Scripts/App/CustomerViewModel.js
+++ Projects/Demo.WebApp/Scripts/App/CustomerViewModel.js
@@ -1,14 +1,34 @@
-﻿function CustomerViewModel(baseUrl) {
+﻿function Customer(id, name, surname, phoneNumber, address, version) {
+    var self = this;
+    self.id = id;
+    self.name = name;
+    self.surname = surname;
+    self.phoneNumber = phoneNumber;
+    self.address = address;
+    self.version = version;
+}
+
+function CustomerViewModel(baseUrl) {
     var self = this;
 
     self.baseUrl = baseUrl;
+
+    self.listVisible = window.ko.observable(true);
+    self.insertOrUpdateVisible = window.ko.observable(false);
+
+    self.customers = window.ko.observableArray([]);
 
     self.testClick = function () {
         alert("ala ma kota");
         ajaxGet("customers/?top=100&skip=0",
             function (data) {
                 alert("ok");
-                alert(data);
+                var x = data.items;
+                var items = [];
+                $.each(x, function (i, item) {
+                    items.push(new Customer(item.id, item.name, item.surname, item.phoneNumber, item.address, item.version));
+                });
+                self.customers(items);
             }),
             function () {
                 alert("error");
